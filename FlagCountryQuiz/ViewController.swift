7538712761a9fc934cae27c3306aaea943004d7e
Henrik789 <henrik789@github.com>
2020-02-07 16:19:51 +0100

    Initial Commit

---
+++ FlagCountryQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlagCountryQuiz
+//
+//  Created by Henrik on 2020-02-07.
+//  Copyright © 2020 Henrik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
