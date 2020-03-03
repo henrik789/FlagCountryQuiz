5f8280777098dffc57724da3a20758a00a91f211
Henrik789 <henrik789@github.com>
2020-03-03 21:11:09 +0100

    edited son

--- FlagCountryQuiz/Screens/Settings/SettingsViewController.swift
+++ FlagCountryQuiz/Screens/Settings/SettingsViewController.swift
@@ -7,22 +7,28 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var settingsBtn: UIButton!
     @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var flagSlider: UISlider!
+    @IBOutlet weak var timeSlider: UISlider!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1000)) {
-                print("still")
-        }
-        
-        headerView.backgroundColor = .myGreen
-        settingsBtn.backgroundColor = .myRed
-        settingsBtn.layer.cornerRadius = settingsBtn.bounds.height / 2
-        mainView.backgroundColor = .myGreen
+        config()
         
     }
     
+    
+    func config() {
+        flagSlider.tintColor = .myBlue
+        flagSlider.thumbTintColor = .myBlue
+        timeSlider.tintColor = .myEmerald
+        
+        headerView.backgroundColor = .myBeige
+        settingsBtn.backgroundColor = .myRed
+        settingsBtn.layer.cornerRadius = settingsBtn.bounds.height / 2
+        mainView.backgroundColor = .myBeige
+    }
     
     @IBAction func settingsBtn(_ sender: Any) {
         navigationController?.popViewController(animated: true)
